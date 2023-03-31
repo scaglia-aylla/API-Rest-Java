b26908664fc920e1bd9ee83e8e0b056471e8527a
Aylla Scaglia <aylla@scaglia.com.br>
2023-03-31 19:42:11 -0300

    Criando um CRUD

---
+++ java-week-api/src/main/java/com/javaweekapi/JavaWeekApiApplication.java
@@ -0,0 +1,13 @@
+package com.javaweekapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWeekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWeekApiApplication.class, args);
+	}
+
+}
---
+++ java-week-api/src/test/java/com/javaweekapi/JavaWeekApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaweekapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWeekApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
